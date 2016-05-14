356162e126c8a4ee68d56cd5f32d0d17fbb37516
The Nhan <luongthenhan@gmail.com>
2016-05-15 02:04:28 +0700

    actionbar spinner

--- app/src/main/java/com/example/thenhan/navigationdrawer/MainActivity.java
+++ app/src/main/java/com/example/thenhan/navigationdrawer/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.thenhan.navigationdrawer;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.content.res.Resources;
 import android.graphics.Bitmap;
@@ -25,7 +26,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -40,6 +43,12 @@ public class MainActivity extends AppCompatActivity
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        // enable the home button
+        //ActionBar actionBar = getActionBar();
+        //actionBar.setHomeButtonEnabled(true);
+        //actionBar.setDisplayUseLogoEnabled(false);
+        //actionBar.setDisplayHomeAsUpEnabled(true);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         if (fab != null) {
@@ -95,6 +104,16 @@ public class MainActivity extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        // Spinner on Action Bar
+        MenuItem item = menu.findItem(R.id.spinner);
+        Spinner spinner = (Spinner) MenuItemCompat.getActionView(item);
+
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.spinner_data, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+
+        spinner.setAdapter(adapter);
 
         // get access to the collapsible SearchView
         searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
